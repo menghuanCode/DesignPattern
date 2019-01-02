e31d83fe2a392fab60b21778ba6b9594922fef70
meng-Macbook <1572482184@qq.com>
2019-01-03 00:02:41 +0800

    '初始化gitbook目录'

---
+++ _book/php/Creaional/Factory/ShapeFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once "Circle.php";
+require_once "Rectangle.php";
+require_once "Square.php";
+
+class ShapeFactory {
+    public function getShape($shapeType) {
+        if ($shapeType == null || !is_string($shapeType)) {
+            return null;
+        }
+
+        if ($shapeType === "CIRCLE") {
+            return new Circle();
+        } else if ($shapeType === "RECTANGLE") {
+            return new Rectangle();
+        } else if ($shapeType === "SQUARE") {
+            return new Square();
+        }
+
+        return null;
+    }
+}
